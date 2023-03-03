0cd894f440ee3c54f40e9b65736bc30ba3f0b7e8
Sri Harsha <sriharsha.g15@iiits.in>
2023-03-02 17:40:03 -0700

    Added to src

---
+++ src/Urinals.java
@@ -0,0 +1,47 @@
+// Author: Sri Harsha Gajavalli
+// ASU ID: 1225891175
+// ASUrite: sgajaval
+// Class: SER 515
+// Last Modified Date: 03/02/2023
+
+
+
+public class Urinals{
+    Boolean goodString(String s){
+        /*int count = s.length();
+        
+        if(count > 0 and count < 16){
+            return false;
+        }
+        return true;*/
+        System.out.println("Not yet implemented");
+        return true;
+    }
+
+    void openFile(){
+        System.out.println("Not yet implemented");
+    }
+
+    int countUrinals(String s){
+        System.out.println("Not yet implemented");
+        return 0;
+    }
+
+    String getString(){
+        System.out.println("Not yet implemented");
+        return "";
+    }
+
+    void closeFile(){
+        System.out.println("Not yet implemented");
+    }
+
+    void writeToFile(String s){
+        System.out.println("Not yet implemented");
+    }
+    
+}
+
+public class public static void main(String[] args) {
+    
+}
